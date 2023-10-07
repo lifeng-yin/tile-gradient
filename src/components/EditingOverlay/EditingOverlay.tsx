0cb66ff125dab65b227033a885de33a0317ad4e1
Li Feng Yin <lifeng-yin@proton.me>
2023-10-06 20:02:22 -0400

    :sparkles: Add editing for four colors

--- src/components/EditingOverlay/EditingOverlay.tsx
+++ src/components/EditingOverlay/EditingOverlay.tsx
@@ -2,21 +2,35 @@ import { RgbaColorPicker, RgbaColor } from "react-colorful"
 import { convertColorToRgba } from "../../utils/colors"
 import { Color } from "../../types/types"
 import { Dispatch, SetStateAction } from "react"
+import { range } from "../../utils/misc"
+
+const ColorPicker = (
+    { colors, setColors, index }:
+    { colors: Color[], setColors: Dispatch<SetStateAction<Color[]>>, index: number }
+) => <RgbaColorPicker
+        className=''
+        color={convertColorToRgba(colors[index])}
+        onChange={(newColor: RgbaColor) => {
+            setColors(colors.map((color, i) => {
+                if (i === index) return Object.values(newColor) as Color
+                else return color
+            }))
+        }}
+    />
 
 const EditingOverlay = (
     { colors, setColors }:
     { colors: Color[], setColors: Dispatch<SetStateAction<Color[]>>
 }) => {
   return (
-    <div>
-        <RgbaColorPicker
-            className='absolute bottom-56 h-1/6'
-            color={convertColorToRgba(colors[0])}
-            onChange={(color: RgbaColor) => {
-                setColors([Object.values(color) as Color, ...colors.slice(1)])
-            }}
-        />
-  </div>
+    <div className="flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-8 w-[32rem] flex-wrap items-center justify-center">
+        { range(4).map(index => <ColorPicker
+            colors={colors}
+            setColors={setColors}
+            index={index}
+        />)}
+        
+    </div>
   )
 }
 
